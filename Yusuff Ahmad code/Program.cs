aff64bd5b6c3752411077a501041ced0d927c750
YusuffAhmad <97904018+YusuffAhmad@users.noreply.github.com>
2022-01-21 12:22:01 +0100

    Code Learners Hub Code file upload

---
+++ Yusuff Ahmad code/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yusuff_Ahmad_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 5;
+          int result = Question1.CoinBase(input);
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ Yusuff Ahmad code/Question1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yusuff_Ahmad_code
+{
+    class Question1
+    {
+        public static int CoinBase(int input)
+        {
+            if (input%2==0)
+            {
+                int count = input / 2;
+                List<int> myList = new List<int>();
+                for (int i = 1; myList.Count == input; i++)
+                {
+
+                    for (int j = i + 1; j == input; j++)
+                    {
+                        myList.Add(i + j);
+                    }
+                    count++;
+                }
+                return count - 1;
+            }
+            else
+            {
+                int count = (input /2) + 1;
+                List<int> myList = new List<int>();
+                for (int i = 1; myList.Count == input; i++)
+                {
+
+                    for (int j = i + 1; j == input; j++)
+                    {
+                        myList.Add(i + j);
+                    }
+                    count++;
+                }
+                return count - 1;
+            }
+            
+        }
+    }
+}
